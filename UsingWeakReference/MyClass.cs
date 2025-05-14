95e3fe170d0499f1bbb903d8e76780a58ef2cf97
Анатолий <homenkotolya2@gmail.com>
2025-05-15 00:32:29 +0700

    Выполнил задачу 4. WeakReference<T> не удерживает объект в памяти. Когда все сильные ссылки удалены, GC может собрать объект. После GC: TryGetTarget вернёт false, если объект уничтожен. IsAlive (у обычной WeakReference) также будет false. Делаем вывод что WeakReference позволяет "наблюдать" за объектом, не препятствуя его удалению сборщиком мусора.

---
+++ UsingWeakReference/MyClass.cs
@@ -0,0 +1,17 @@
+﻿namespace UsingWeakReference
+{
+    class MyClass
+    {
+        public string Name { get; }
+
+        public MyClass(string name)
+        {
+            Name = name;
+        }
+
+        ~MyClass()
+        {
+            Console.WriteLine($"Финализатор вызван для {Name}");
+        }
+    }
+}
---
+++ UsingWeakReference/Program.cs
@@ -0,0 +1,32 @@
+﻿using UsingWeakReference;
+class Program
+{
+    static void Main()
+    {
+        WeakReference<MyClass> weakRef;
+
+        {
+            var obj = new MyClass("TestObject");
+            weakRef = new WeakReference<MyClass>(obj);
+
+            Console.WriteLine("Объект создан и слабая ссылка сохранена.");
+
+            obj = null;
+        }
+
+        Console.WriteLine("Сильная ссылка удалена. Форсируем сборку мусора...");
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        if (weakRef.TryGetTarget(out MyClass target))
+        {
+            Console.WriteLine($"Объект ещё жив: {target.Name}");
+        }
+        else
+        {
+            Console.WriteLine("Объект был собран GC.");
+        }
+    }
+}
