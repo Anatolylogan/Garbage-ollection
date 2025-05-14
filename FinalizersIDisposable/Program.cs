3b74b2344e99a23ce6480a9f6a88a0f32be55bc1
Анатолий <homenkotolya2@gmail.com>
2025-05-15 00:12:28 +0700

    Выполнил задачу 2 .Финализатор вызывается только сборщиком мусора — и только если объект не был удалён с помощью Dispose() и GC.SuppressFinalize(). Dispose() вызывается вручную, чтобы освободить ресурсы немедленно, без ожидания GC.  GC.SuppressFinalize(this) отключает финализацию для объекта .GC.ReRegisterForFinalize(this) повторно включает финализацию, если она была подавлена.

---
+++ FinalizersIDisposable/Program.cs
@@ -0,0 +1,45 @@
+﻿using FinalizersIDisposable;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Без Dispose ===");
+        CreateWithoutDispose();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        Console.WriteLine();
+
+        Console.WriteLine("=== С Dispose ===");
+        CreateWithDispose();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        Console.WriteLine();
+
+        Console.WriteLine("=== С ReRegisterForFinalize ===");
+        CreateWithReRegister();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        Console.ReadLine();
+    }
+
+    static void CreateWithoutDispose()
+    {
+        var obj = new ResourceHolder("Obj1");
+    }
+
+    static void CreateWithDispose()
+    {
+        var obj = new ResourceHolder("Obj2");
+        obj.Dispose();
+    }
+
+    static void CreateWithReRegister()
+    {
+        var obj = new ResourceHolder("Obj3");
+        obj.Dispose();                  
+        obj.EnableFinalizerAgain();     
+    }
+}
---
+++ FinalizersIDisposable/ResourceHolder.cs
@@ -0,0 +1,40 @@
+﻿namespace FinalizersIDisposable
+{
+    class ResourceHolder : IDisposable
+    {
+        private bool _disposed = false;
+        private string _name;
+
+        public ResourceHolder(string name)
+        {
+            _name = name;
+            Console.WriteLine($"Создан объект: {_name}");
+        }
+
+        ~ResourceHolder()
+        {
+            Console.WriteLine($"Финализатор вызван: {_name}");
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Console.WriteLine($"Dispose вызван: {_name}");
+                _disposed = true;
+
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        public void EnableFinalizerAgain()
+        {
+            if (_disposed)
+            {
+                Console.WriteLine($"ReRegisterForFinalize: {_name}");
+                GC.ReRegisterForFinalize(this);
+                _disposed = false;
+            }
+        }
+    }
+}
