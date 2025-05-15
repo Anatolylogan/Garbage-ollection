cad9f13dd91727a3f1f0b859ac91ad90ac4826c3
Анатолий <homenkotolya2@gmail.com>
2025-05-15 23:43:45 +0700

    Выполнил задачу 5. В .NET, объекты размером более 85 000 байт автоматически попадают в Large Object Heap (LOH). LOH всегда считается Generation 2.  GC.Collect() по умолчанию очищает только поколение 0 и 1, но если мы вызываем GC.Collect() с GC.Collect(2), то очищается также и LOH.

---
+++ LOH/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Создаём большой объект (>85 000 байт)...");
+
+        byte[] largeArray = new byte[100_000];
+
+        Console.WriteLine("Поколение объекта: " + GC.GetGeneration(largeArray));
+        largeArray = null;
+
+        Console.WriteLine("Удалена сильная ссылка на объект. Запускаем сборку мусора...");
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        Console.WriteLine("Сборка мусора завершена. LOH собирается только при полной сборке (Gen 2).");
+    }
+}
+
